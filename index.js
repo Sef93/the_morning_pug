1d8de13f9b8d16c6bd2498a9a9d023b8478812fd
Sef93 <kurtossyo93@gmail.com>
2017-02-20 15:54:29 +0100

    adding stuff

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var bodyParser = require('body-parser');  
 var request = require('request');  
 var app = express();
+var mysql = require('mysql');
+var myTimers = require('schedule');
 
 app.use(bodyParser.urlencoded({extended: false}));  
 app.use(bodyParser.json());  
@@ -9,7 +11,7 @@ app.listen((process.env.PORT || 3000));
 
 // Server frontpage
 app.get('/', function (req, res) {  
-    res.send('This is TestBot Server');
+    res.send('Es ist uno Testbotto szervero');
 });
 
 // Facebook Webhook
@@ -19,4 +21,19 @@ app.get('/webhook', function (req, res) {
     } else {
         res.send('Invalid verify token');
     }
+});
+
+app.post('/webhook', function (req, res) {
+    var events = req.body.entry[0].messaging;
+    for (i = 0; i < events.length; i++) {
+        var event = events[i];
+        if (event.message && event.message.text) {
+			var szoveg = event.message.text;
+			var kuldoId = event.sender.id;
+			console.log("event: ", event);	
+        }else if(event.message && event.message.attachments){
+			sendMessage(event.sender.id, {text: "Milyen szép kép"});
+		}
+    }
+    res.sendStatus(200);
 });
