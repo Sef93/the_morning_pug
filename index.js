8599eba3f752126387cf392c7a51d92496ad5cb1
Sef93 <kurtossyo93@gmail.com>
2017-02-20 16:03:00 +0100

    adding stuff

--- index.js
+++ index.js
@@ -23,17 +23,14 @@ app.get('/webhook', function (req, res) {
     }
 });
 
-app.post('/webhook', function (req, res) {
+app.post('/webhook', function (req, res) {  
     var events = req.body.entry[0].messaging;
     for (i = 0; i < events.length; i++) {
         var event = events[i];
         if (event.message && event.message.text) {
-			var szoveg = event.message.text;
-			var kuldoId = event.sender.id;
-			console.log("event: ", event);	
-        }else if(event.message && event.message.attachments){
-			sendMessage(event.sender.id, {text: "Milyen szép kép"});
-		}
+            sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
+			console.log(event);
+        }
     }
     res.sendStatus(200);
 });
