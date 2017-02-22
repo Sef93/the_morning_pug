8505810de01a05ad5bb5e7c361702a23d5dcff21
Sef93 <kurtossyo93@gmail.com>
2017-02-22 13:17:48 +0100

    payload_test

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ function init(kuldoId, message) {
 
 function askForSub(kuldoId) {
     connection.query("UPDATE myUsers SET last_command = 'sub' where messageId = '" + kuldoId + "';");
-    sendWannaSub(kuldoId);
+    sendWannaSub(kuldoId, name);
     console.log();
 }
 
